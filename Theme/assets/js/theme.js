a5cebbe50279d05da8c97b37aaf24544844a42ea
Seimith <seimith@gmail.com>
2013-12-22 18:07:16 -0800

    Adde JS

--- Theme/assets/js/theme.js
+++ Theme/assets/js/theme.js
@@ -4,11 +4,25 @@
 !function ($) {
 
     $('#myCarousel').carousel()
-    
+
 }(window.jQuery)
 
 !function ($) {
 
     $('#myCarousel2').carousel()
-    
+
 }(window.jQuery)
+
+// ///////////////
+
+$(document).on('ready', function() {
+  $('#bio').hide();
+})
+
+$('body').on('mouseover', '#student_img', function() {
+  $('p#bio').slideDown();
+})
+
+$('#student_img').on('mouseout', function() {
+  $('p#bio').slideUp();
+})
