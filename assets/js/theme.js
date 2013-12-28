ae5054777e13388eba116d3a84af808e0c708091
james <jamkcas@yahoo.com>
2013-12-28 11:12:42 -0800

    added functionality to display the currently selected students info in the display window on mouseover

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,11 +1,24 @@
+
+
+
 $(document).ready(function() {
   $(".content").hide();
   $("#studentdisplay").hide();
   //toggle the componenet with class msg_body
   $(".heading").mouseover(function() {
+    // Grabbing the current student info
+    var name = $(this).context.innerText;
+    var pic = $(this).next().children()[0].src;
+    var bio = $(this).next().children()[1].innerHTML;
+
+    // Setting the display info to the current student's info
+    $('.studentName').text(name);
+    $('.studentBio').text(bio);
+    $('.studentPic').attr('src', pic);
+    // $(this).next(".content").slideToggle(300);
+
     $("#meetstudents").hide();
     $("#studentdisplay").show();
-    // $(this).next(".content").slideToggle(300);
   });
   $(".heading").mouseout(function() {
     $("#meetstudents").show();
